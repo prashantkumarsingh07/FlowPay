38aedadfafe77e4d0df44972e962021efaed7796
Prashant Kumar Singh <99783341+prashantkumarsingh07@users.noreply.github.com>
2024-11-23 12:57:12 +0530

    Update Button.jsx

--- My batua/frontend/src/componets/Button.jsx
+++ My batua/frontend/src/componets/Button.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-export const Button = ({ label, onClick }) => {
+export const Button(donate) = ({ label, onClick }) => {
   return (
     <button
       className='w-full rounded-lg font-medium hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 px-5 bg-gray-800 text-white py-2.5  mb-2'
